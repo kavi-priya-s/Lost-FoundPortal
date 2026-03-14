94adefd04f20ccfa2b08f33c849ce45304d5619d
Kavipriya <163543481+kavi-priya-s@users.noreply.github.com>
2026-03-14 15:34:38 +0530

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,59 @@
+package com.project.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="item")
+public class Item {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String item_name;
+	private String description;
+	private String location;
+	private String status;
+	private String contact;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getItem_name() {
+		return item_name;
+	}
+	public void setItem_name(String item_name) {
+		this.item_name = item_name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+}
---
+++ footer.jsp
@@ -0,0 +1,7 @@
+<footer class="text-center text-secondary mt-5 mb-3">
+	
+	<hr style = "border-color:#444">
+	
+	<p>© 2026 Lost & Found Portal </p>
+	
+</footer>
